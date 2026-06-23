087ec5344fc75aaa217718e8119fa4df9a8ff24b
mdsrabonosd <mdsrabon.cse@gmail.com>
2026-06-23 22:19:07 +0600

    Web

---
+++ Hotel Mangement Api/Application.unittest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Application.unittest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- Hotel Mangement Api/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Hotel Mangement Api/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd694c27745cb8216b27f4cf84a892ace80f980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee20d2a898084250735818af02da65e08e6acb34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
